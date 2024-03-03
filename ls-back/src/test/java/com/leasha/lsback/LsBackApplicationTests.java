9f192b4adccd614e9143d3061cf47921b191bf52
Lydie CHAUMET <lydie.chaumet@ouidou.fr>
2024-03-03 17:39:06 +0100

    add back-end with Spring Boot

---
+++ ls-back/src/test/java/com/leasha/lsback/LsBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leasha.lsback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LsBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
